12ffd841e99f313af4f1c9a2506f5d21be000aa4
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-09-03 19:43:00 -0400

    Refactor the MessageReceiver and MessageSender classes

--- src/rabbit/message_receiver.py
+++ src/rabbit/message_receiver.py
@@ -1,36 +1,113 @@
 """Class for receiving messages."""
 
+import pika
+import textwrap
 from typing import Callable
-from .rabbitmq import RabbitMQ
+from .config import get_config
 
 
 class MessageReceiver:
     """A class to receive messages."""
 
     def __init__(self):
-        broker = RabbitMQ()
-        broker.connect()
-        broker.show_config()
-        self.broker = broker
+        config = get_config()
+
+        self.username = config["username"]
+        self.password = config["password"]
+        self.host = config["host"]
+        self.port = config["port"]
+        self.connection = None
+        self.channel = None
+
+        self._show_config()
+        self._connect()
+
+    def _show_config(self):
+        """Show configuration."""
+        config_description = f"""
+        Configuration for RabbitMQ is the following:
+        username    {self.username}
+        password    {self.password}
+        host        {self.host}
+        port        {self.port}
+        """
+
+        print(textwrap.dedent(config_description))
+
+    def _connect(self):
+        """Establish a connection."""
+        creds = pika.PlainCredentials(self.username, self.password)
+        params = pika.ConnectionParameters(credentials=creds)
+        self.connection = pika.BlockingConnection(params)
+        self.channel = self.connection.channel()
+
+    def close(self):
+        """Close the connection."""
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     def consume(self):
         """Consume messages from sender."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
 
         def callback(channel, method, properties, body):
-            print(f"○ Received message {body}")
-            print("● Done\n")
+            print(f"[.] Received message {body}")
+            print("[x] Done\n")
 
-        self.broker.consume(queue_name="message", callback=callback)
+        queue_name = "message"
+        self.channel.queue_declare(queue=queue_name)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        print("[.] Waiting for sender. Press CTRL+C to exit.\n")
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        self.close()
+        print("\n[x] Stopped consuming and closed connection.")
 
     def consume_callback(self, callback: Callable):
         """Consume messages from sender using provided callback function."""
-        self.broker.consume(queue_name="message", callback=callback)
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
+
+        queue_name = "message"
+        self.channel.queue_declare(queue=queue_name)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        print("[.] Waiting for sender. Press CTRL+C to exit.\n")
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        self.close()
+        print("\n[x] Stopped consuming and closed connection.")
 
     def consume_emit(self):
-        """Consume messages from sender using exchange fanout."""
+        """Consume messages from sender via exchange using fanout."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
 
         def callback(channel, method, properties, body):
-            print(f"○ Received message {body}")
-            print("● Done\n")
+            print(f"[.] Received message {body}")
+            print("[x] Done\n")
 
-        self.broker.consume_fanout(exchange="emit_message", callback=callback)
+        exchange = "emit_message"
+        self.channel.exchange_declare(exchange=exchange, exchange_type="fanout")
+
+        result = self.channel.queue_declare(queue="", exclusive=True)
+        queue_name = result.method.queue
+        self.channel.queue_bind(exchange=exchange, queue=queue_name)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        print("[.] Waiting for sender. Press CTRL+C to exit.\n")
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        self.close()
+        print("\n[x] Stopped consuming and closed connection.")
--- src/rabbit/message_sender.py
+++ src/rabbit/message_sender.py
@@ -1,28 +1,68 @@
 """Class for sending messages."""
 
-from .rabbitmq import RabbitMQ
+import pika
+import textwrap
+from .config import get_config
+
 
 class MessageSender:
     """A class to send messages."""
 
     def __init__(self, show_config=False):
-        self.broker = RabbitMQ()
+        config = get_config()
+
+        self.username = config["username"]
+        self.password = config["password"]
+        self.host = config["host"]
+        self.port = config["port"]
+        self.connection = None
+        self.channel = None
 
         if show_config:
-            self.broker.show_config()
+            self.show_config()
+
+    def show_config(self):
+        """Show configuration."""
+        config_description = f"""
+        Configuration for RabbitMQ is the following:
+        username    {self.username}
+        password    {self.password}
+        host        {self.host}
+        port        {self.port}
+        """
+
+        print(textwrap.dedent(config_description))
 
     def connect(self):
-        """Connect to the RabbitMQ broker."""
-        self.broker.connect()
+        """Establish a connection."""
+        creds = pika.PlainCredentials(self.username, self.password)
+        params = pika.ConnectionParameters(credentials=creds)
+        self.connection = pika.BlockingConnection(params)
+        self.channel = self.connection.channel()
 
     def close(self):
         """Close the connection."""
-        self.broker.close()
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     def send_message(self, msg: str):
-        """Send a message."""
-        self.broker.publish(queue_name="message", body=msg)
+        """Send a message via publish method."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
+
+        queue_name = "message"
+        self.channel.queue_declare(queue=queue_name)
+        self.channel.basic_publish(exchange="", routing_key=queue_name, body=msg)
+
+        print(f"[x] Sent '{msg}' to queue '{queue_name}'")
 
     def emit_message(self, msg: str):
-        """Send message to all message receivers."""
-        self.broker.publish_fanout(exchange="emit_message", body=msg)
+        """Send message to all message receivers via exchange using fanout."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
+
+        exchange = "emit_message"
+        self.channel.exchange_declare(exchange=exchange, exchange_type="fanout")
+        self.channel.basic_publish(exchange=exchange, routing_key="", body=msg)
+
+        print(f"[x] Sent '{msg}' to exchange '{exchange}'")
