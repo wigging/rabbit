1caced49851aba9a814f14b3de3beca9b7031b45
Gavin Wiggins <6828967+wigging@users.noreply.github.com>
2024-09-04 11:13:35 -0400

    Refactor the PythonSender and PythonReceiver classes

--- src/rabbit/python_receiver.py
+++ src/rabbit/python_receiver.py
@@ -1,40 +1,100 @@
 """Class for receiving Python commands."""
 
+import pika
+import textwrap
 import subprocess
-from .rabbitmq import RabbitMQ
+from .config import get_config
 
 
 class PythonReceiver:
     """A class to receive Python commands."""
 
     def __init__(self):
-        broker = RabbitMQ()
-        broker.connect()
-        broker.show_config()
-        self.broker = broker
+        config = get_config()
+
+        self.username = config["username"]
+        self.password = config["password"]
+        self.host = config["host"]
+        self.port = config["port"]
+        self.connection = None
+        self.channel = None
+
+        self._show_config()
+        self._connect()
+
+    def _show_config(self):
+        """Show configuration."""
+        config_description = f"""
+        Configuration for RabbitMQ is the following:
+        username    {self.username}
+        password    {self.password}
+        host        {self.host}
+        port        {self.port}
+        """
+
+        print(textwrap.dedent(config_description))
+
+    def _connect(self):
+        """Establish a connection."""
+        creds = pika.PlainCredentials(self.username, self.password)
+        params = pika.ConnectionParameters(credentials=creds)
+        self.connection = pika.BlockingConnection(params)
+        self.channel = self.connection.channel()
+
+    def close(self):
+        """Close the connection."""
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     def consume(self):
-        """Run Python commands from sender."""
+        """Consume Python commands from sender."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
 
         def callback(channel, method, properties, body):
             cmd = body.decode().strip()
-            print(f"○ Run the Python command 'python {cmd}'")
+            print(f"[.] Run the Python command 'python {cmd}'")
 
             subprocess.run(["python", cmd])
-            print("● Done\n")
+            print("[x] Done\n")
 
-        self.broker.consume(queue_name="python", callback=callback)
+        queue_name = "python"
+        self.channel.queue_declare(queue=queue_name)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback, auto_ack=True)
+        print("[.] Waiting for sender. Press CTRL+C to exit.\n")
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        self.close()
+        print("\n[x] Stopped consuming and closed connection.")
 
     def consume_task(self):
-        """Run Python tasks from sender."""
+        """Consume Python tasks from sender."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
 
         def callback(channel, method, properties, body):
             cmd = body.decode().strip()
-            print(f"○ Run the Python task 'python {cmd}'")
+            print(f"[.] Run the Python task 'python {cmd}'")
 
             subprocess.run(["python", cmd])
-            print("● Done\n")
+            print("[x] Done\n")
 
             channel.basic_ack(delivery_tag=method.delivery_tag)
 
-        self.broker.consume_durable(queue_name="python_task", callback=callback)
+        queue_name = "python_task"
+        self.channel.queue_declare(queue=queue_name, durable=True)
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(queue=queue_name, on_message_callback=callback)
+        print("[.] Waiting for sender. Press CTRL+C to exit.\n")
+
+        try:
+            self.channel.start_consuming()
+        except KeyboardInterrupt:
+            self.channel.stop_consuming()
+
+        self.close()
+        print("\n[x] Stopped consuming and closed connection.")
--- src/rabbit/python_sender.py
+++ src/rabbit/python_sender.py
@@ -1,29 +1,69 @@
 """Class for sending Python commands."""
 
-from .rabbitmq import RabbitMQ
-
+import pika
+import textwrap
+from .config import get_config
 
 class PythonSender:
     """A class to send Python commands."""
 
     def __init__(self, show_config=False):
-        self.broker = RabbitMQ()
+        config = get_config()
+
+        self.username = config["username"]
+        self.password = config["password"]
+        self.host = config["host"]
+        self.port = config["port"]
+        self.connection = None
+        self.channel = None
 
         if show_config:
-            self.broker.show_config()
+            self._show_config()
+
+    def _show_config(self):
+        """Show configuration."""
+        config_description = f"""
+        Configuration for RabbitMQ is the following:
+        username    {self.username}
+        password    {self.password}
+        host        {self.host}
+        port        {self.port}
+        """
+
+        print(textwrap.dedent(config_description))
 
     def connect(self):
         """Connect to the RabbitMQ broker."""
-        self.broker.connect()
+        creds = pika.PlainCredentials(self.username, self.password)
+        params = pika.ConnectionParameters(credentials=creds)
+        self.connection = pika.BlockingConnection(params)
+        self.channel = self.connection.channel()
 
     def close(self):
         """Close the RabbitMQ broker connection."""
-        self.broker.close()
+        if self.connection and self.connection.is_open:
+            self.connection.close()
 
     def run_command(self, cmd: str):
         """Run a Python command."""
-        self.broker.publish(queue_name="python", body=cmd)
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
+
+        queue_name = "python"
+        self.channel.queue_declare(queue=queue_name)
+        self.channel.basic_publish(exchange="", routing_key=queue_name, body=cmd)
+
+        print(f"[x] Sent '{cmd}' to queue '{queue_name}'")
 
     def run_task(self, task: str):
-        """Run a Python task."""
-        self.broker.publish_durable(queue_name="python_task", body=task)
+        """Run a Python task using a durable queue."""
+        if not self.channel:
+            raise Exception("Uh oh! Connection is not established.")
+
+        queue_name = "python_task"
+        self.channel.queue_declare(queue=queue_name, durable=True)
+
+        props = pika.BasicProperties(delivery_mode=pika.DeliveryMode.Persistent)
+        self.channel.basic_publish(exchange="", routing_key=queue_name, body=task, properties=props)
+
+        print(f"[x] Sent '{task}' to durable queue '{queue_name}'")
